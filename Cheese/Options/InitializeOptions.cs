772c1ec1b6cfad9971ca87bc0b68f56b693dfb37
Dynesshely <catrol@qq.com>
2024-03-17 21:35:39 +0800

    💾 Feat(Project): Optimized.

---
+++ Cheese/Options/InitializeOptions.cs
@@ -0,0 +1,10 @@
+﻿using CommandLine;
+
+namespace Cheese.Options;
+
+[Verb("init", HelpText = "Initialize a repo env.")]
+public class InitializeOptions : Options
+{
+    [Option('t', "initialize-target", Required = false, Default = null, HelpText = "Indicate initialize target.")]
+    public string? InitializeTarget { get; set; }
+}
